49345e743c4149addf9c6b81670c9fe2b98912c0
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-08-31 17:40:32 +0300

    cp

---
+++ CodeForces/(A) Dora's Set.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int m , n;
+void solve() {
+
+
+    cin >> m >> n ;
+    cout<<((n+1)/2-m/2)/2<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+     cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve();
+    }
+
+    return 0;
+}
