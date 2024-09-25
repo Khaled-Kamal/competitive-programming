d4e158a1914b9df0e57fcf9a531883be7fe1b200
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-09-25 15:35:07 +0300

    Create I. Emad vs Joker.cpp

---
+++ ICPC Mansoura  Level 0 training  Public/contests/Contest #2/I. Emad vs Joker.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+
+    int n ;
+    cin >> n ;
+
+   if(n % 4 == 0)
+   {
+       cout << "Joker";
+   }
+   else
+   {
+       cout << "Emad";
+   }
+
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1 ;
+ //   cin >> tc ;
+    for(int i = 1 ; i <= tc ; i++)
+    {
+        solve();
+    }
+
+}
