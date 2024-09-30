ba925f5b238f8fcf8b6f8ffac3b800a0b7885760
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-09-30 15:39:42 +0300

    Update two-sum.cpp

--- Leetcode/two-sum.cpp
+++ Leetcode/two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& v, int target) {
         map<int,int>m,indx;
-        vector<int>ans(2);
+        vector<int>anss(2);
         for (int i = 0; i < v.size(); ++i) {
             if (m[v[i]]){
                 ans[0]=i;
@@ -12,6 +12,6 @@ public:
             m[target-v[i]]=1;
             indx[v[i]]=i;
         }
-        return ans;
+        return anss;
     }
 };
