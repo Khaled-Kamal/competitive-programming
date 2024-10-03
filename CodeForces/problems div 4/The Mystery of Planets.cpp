a662563b69481c99603008bf0aa38f1ddcd0c6d9
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-10-03 17:53:55 +0300

    Update The Mystery of Planets.cpp

--- CodeForces/problems div 4/The Mystery of Planets.cpp
+++ CodeForces/problems div 4/The Mystery of Planets.cpp
@@ -1,1 +1,26 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+void solve()
+{
+    string x;
+    cin >> x;
+
+
+    cout << x << " x 10^22 kilograms" ;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+  //  cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
