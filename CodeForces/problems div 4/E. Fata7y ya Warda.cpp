baf21e63cbd92263c8a699c3d8d54fb669d7e030
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-10-07 01:42:10 +0300

    Update E. Fata7y ya Warda.cpp

--- CodeForces/problems div 4/E. Fata7y ya Warda.cpp
+++ CodeForces/problems div 4/E. Fata7y ya Warda.cpp
@@ -1,1 +1,28 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+
+
+    int x , y ;
+    cin >> x >> y ;
+
+    cout << (y == x + 1 || (x == 1 && y == 10) ? "YES" : "NO") ;
+
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
