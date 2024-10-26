07f45a4f38c1cb4f4e729afc234e60239c893a49
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-10-26 14:57:08 +0300

    Update A. Sakurako and Kosuke.cpp

--- CodeForces/problems div 3/A. Sakurako and Kosuke.cpp
+++ CodeForces/problems div 3/A. Sakurako and Kosuke.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+      int t  ;
+      cin >> t ;
+      while(t--)
+      {
+          int n ;
+          cin >> n ;
+
+          cout << (n&1 ? "Kosuke" : "Sakurako") << '\n';
+      }
+
+}
 
