d48cb7229337b2a291ff96012df709b5aa49dae1
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-10-27 15:20:22 +0300

    Update A. Calculating Function.cpp

--- CodeForces/problems div 2/A. Calculating Function.cpp
+++ CodeForces/problems div 2/A. Calculating Function.cpp
@@ -1,1 +1,26 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int f(int n)
+{
+     if(n&1)
+     {
+         return -(n + 1) / 2;
+     }
+     else
+     {
+         return n / 2 ;
+     }
+}
+signed main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+     int n ;
+     cin >> n ;
+     cout << f(n) ;
+}
