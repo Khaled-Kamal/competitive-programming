b8c42563c3b2e64c9b4c470baf673f6be881516f
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-11-23 22:35:05 +0200

    Update L. Max Subarray.cpp

--- Assiut University Training - Newcomers/sheets/Sheet #3 (Arrays)/L. Max Subarray.cpp
+++ Assiut University Training - Newcomers/sheets/Sheet #3 (Arrays)/L. Max Subarray.cpp
@@ -14,7 +14,7 @@ void solve(){
     }
 
     for (int i = 0; i < n ; ++i) {
-       int ans = 0 ;
+       int ans = a[i] ;
         for (int j = i; j < n ; ++j) {
             ans = max(ans , a[j]);
             cout << ans << ' ' ;
