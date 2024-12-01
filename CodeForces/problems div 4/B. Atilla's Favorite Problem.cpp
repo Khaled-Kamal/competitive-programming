2d732e2976c6ca6180a381afab79bdafb26bd891
Khaled Kamal Bdar <KhaledKamal1555@gmail.com>
2024-12-01 16:06:01 +0200

    Update B. Atilla's Favorite Problem.cpp

--- CodeForces/problems div 4/B. Atilla's Favorite Problem.cpp
+++ CodeForces/problems div 4/B. Atilla's Favorite Problem.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+#define int long long
+#define double long double
+
+void solve() {
+
+    int n ;
+    cin >> n ;
+    string s ;
+    cin >> s ;
+
+    int mx = *max_element(s.begin() ,s.end()) - 'a' + 1 ;
+
+    cout << (int)(mx);
+
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1 ; cin >> tc ;
+    for(int i = 1 ; i <= tc ; i++)
+    {
+        solve();
+        cout << '\n';
+    }
+    return (0 - 0);
+
+}
